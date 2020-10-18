ed485df4280ee528e87868ba19969785b0350c47
rameshbabu <vardohan.ramesh@gmail.com>
2020-10-19 00:07:49 +0530

    t-august

--- code/library/boring_stuff.py
+++ code/library/boring_stuff.py
@@ -1,27 +1,31 @@
 import os
 import sys
 
+
+def mycopy():
+    try:
+        pass
+    except:
+        print(sys.exc_info())
+
+
+def mymove():
+    try:
+        pass
+    except:
+        print(sys.exc_info())
+
+
 class boring_stuff():
     def __init__(self, source='', destination='', keyword=''):
         self.source = source
         self.destination = destination
         self.keyword = keyword
 
-    def mycopy(self):
-        try:
-            pass
-        except:
-            print(sys.exc_info())
-        
-    def mymove(self):
-        try:
-            pass
-        except:
-            print(sys.exc_info())
-
     def __str__(self):
         return "boring_stuff"
-        
+
+
 class mymodule():
     def __init__(self, module=''):
         self._module = module
@@ -39,16 +43,16 @@ class mymodule():
 
     def details(self, mycallable=False):
         try:
-            mydir               = dir(self._module)
-            callable_data       = []
-            non_callable_data   = []
-            
+            mydir = dir(self._module)
+            callable_data = []
+            non_callable_data = []
+
             for name in mydir:
                 if callable(eval(str(self._module).split()[1][1:-1] + '.' + name)):
                     callable_data.append(name)
                 else:
                     non_callable_data.append(name)
-            
+
             if mycallable:
                 print(f"##### {str(self._module).split()[1][1:-1]}: Callable #####")
                 count = 1
--- main/main.py
+++ main/main.py
@@ -1,19 +1,20 @@
 import sys
+
 sys.path.append("/home/ramesh/Documents/github/vardohan_python/code/global_libraries")
 sys.path.append("/home/ramesh/Documents/github/vardohan_python/code/local_libraries")
 
-from boring_stuff import boring_stuff
 from boring_stuff import mymodule
+from boring_stuff import common_stuff
 
-#bs = boring_stuff()
-#print(bs)
-#print(bs.__dict__)
+# bs = boring_stuff()
+# print(bs)
+# print(bs.__dict__)
 
-#print(sys.path)
+# print(sys.path)
 
 mm = mymodule(sys)
 mm.details(mycallable=False)
 
-#import os
-#mm.name = os
-#mm.details()
+# import os
+# mm.name = os
+# mm.details()
